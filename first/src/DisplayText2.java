b2f7eebea4272f6c1b89f0e2fc76f3a9d8b25e36
Sommai-Supawong <Sommai>
2024-06-28 11:23:57 +0700

    first time

---
+++ first/src/DisplayText2.java
@@ -0,0 +1,10 @@
+public class DisplayText2 {
+    public static void main(String[] args) {
+        
+    System.out.println("m");   
+    System.out.println("a");
+    System.out.println("i");
+    System.out.println("y");
+    System.out.println(1+11+11+1+1+1+1+1+1);
+}
+}
